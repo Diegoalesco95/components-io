8904e34657ba93035ee86c0ffc2d67642bd50176
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2022-06-15 17:38:36 -0500

    :sparkles: Add navigation

---
+++ src/app/pages/actions-sheet/actions-sheet.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actions-sheet',
+  templateUrl: './actions-sheet.page.html',
+  styleUrls: ['./actions-sheet.page.scss'],
+})
+export class ActionsSheetPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/alert/alert.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ComponentsModule } from 'src/app/components/components.module';
+
+import { AlertPageRoutingModule } from './alert-routing.module';
+
+import { AlertPage } from './alert.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    AlertPageRoutingModule,
+    ComponentsModule,
+  ],
+  declarations: [AlertPage],
+})
+export class AlertPageModule {}
