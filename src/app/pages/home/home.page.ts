bb948b1a367d5bdb474a01444ab34b9ce53e9cf0
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2022-06-16 08:18:10 -0500

    :sparkles: Add list

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IComponent {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  components: IComponent[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Actions Sheet',
+      redirectTo: '/actions-sheet',
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
